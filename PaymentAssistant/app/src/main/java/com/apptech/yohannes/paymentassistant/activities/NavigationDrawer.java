2a0b88ffd54bbce4adf04bfec843c55ffba4d4b4
Yohannes Tamru <yohannist@gmail.com>
2014-08-28 02:45:09 +0300

    compiles fine

--- PaymentAssistant/app/src/main/java/com/apptech/yohannes/paymentassistant/activities/NavigationDrawer.java
+++ PaymentAssistant/app/src/main/java/com/apptech/yohannes/paymentassistant/activities/NavigationDrawer.java
@@ -5,21 +5,17 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+
 import com.apptech.yohannes.paymentassistant.R;
 
-public class drawer extends Activity
+public class NavigationDrawer extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
     /**
@@ -138,7 +134,7 @@ public class drawer extends Activity
         @Override
         public void onAttach(Activity activity) {
             super.onAttach(activity);
-            ((drawer) activity).onSectionAttached(
+            ((NavigationDrawer) activity).onSectionAttached(
                     getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
