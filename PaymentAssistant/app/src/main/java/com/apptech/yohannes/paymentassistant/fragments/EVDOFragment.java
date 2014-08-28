68e67cb3d3cea266bb2c39b871a477bcfe9ab60b
Yohannes Tamru <yohannist@gmail.com>
2014-08-29 00:15:47 +0300

    EVDO fragment added

---
+++ PaymentAssistant/app/src/main/java/com/apptech/yohannes/paymentassistant/fragments/EVDOFragment.java
@@ -0,0 +1,19 @@
+package com.apptech.yohannes.paymentassistant.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Yohannes on 8/29/2014.
+ */
+public class EVDOFragment extends Fragment {
+
+    @Override
+    public View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle)
+    {
+    
+    }
+}
