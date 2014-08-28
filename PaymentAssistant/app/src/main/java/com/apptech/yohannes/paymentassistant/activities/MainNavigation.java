ec687ef587f576f99791d21260c78b59f77b21e3
Yohannes Tamru <yohannist@gmail.com>
2014-08-28 13:30:45 +0300

    list of menu items added to string array navigation activity view set up

--- PaymentAssistant/app/src/main/java/com/apptech/yohannes/paymentassistant/activities/MainNavigation.java
+++ PaymentAssistant/app/src/main/java/com/apptech/yohannes/paymentassistant/activities/MainNavigation.java
@@ -1,7 +1,27 @@
 package com.apptech.yohannes.paymentassistant.activities;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+import com.apptech.yohannes.paymentassistant.R;
+
 /**
  * Created by Yohannes on 8/28/2014.
  */
-public class MainNavigation {
+public class MainNavigation extends Activity {
+    private FrameLayout mainContentView;
+    private ListView drawerMenu;
+
+    @Override
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+
+        setContentView(R.layout.activity_main_navigation);
+
+        mainContentView = (FrameLayout) findViewById(R.id.mainContent);
+        drawerMenu = (ListView)findViewById(R.id.navigationDrawerList);
+
+    }
 }
